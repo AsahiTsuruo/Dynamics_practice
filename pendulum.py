1ee5cba5708a445880cf3a6680cc042abcec5828
AsahiTsuruo <a.tsuruo74@gmail.com>
2019-09-05 14:40:28 +0900

    振り子

--- pendulum.py
+++ pendulum.py
@@ -16,4 +16,27 @@ th1 = 30.0  #角度の初期値[deg]
 w1 = 0.0    #角速度の初期値[deg]
 
 #初期状態
-state = np.ra
+state = np.radians([th1,w1])
+dt = 0.05
+t = np.arange(0.0,20,dt)
+sol = odeint(func,state,t)
+
+theta = sol[:,0]
+x = L * sin(theta)
+y = - L * cos(theta)
+
+fig = plt.figure()
+ax = fig.add_subplot(111,autoscale_on=False,xlim=(-L,L),ylim=(-L,L))
+ax.set_aspect("equal")
+ax.grid()
+
+line, = ax.plot([],[],"-o",lw=2)
+
+def animate(i):
+    thisx = [0,x[i]]
+    thisy = [0,y[i]]
+
+    line.set_data(thisx,thisy)
+    return line,
+ani = FuncAnimation(fig,animate,frames=np.arange(0, len(t)),interval=25,  blit = True)
+plt.show()
